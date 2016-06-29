7071607a84b9138566355cb3a4c761fb26c9fc7a
Israel Zúñiga de la Mora <israelzuniga@users.noreply.github.com>
2016-06-29 17:26:53 -0500

    Placed REST API for clients and connection to 21
    - api.py: RESTful API to vote on proposals
    
    - connector.py: Functions to charge one satoshi per submitted vote on a
    proposal

---
+++ src/api.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify, request, abort
+from two1.wallet import Wallet
+from two1.bitrequests import BitTransferRequests
+
+# set up bitrequest client for BitTransfer requests
+wallet = Wallet()
+requests = BitTransferRequests(wallet)
+
+# server address
+server_url = 'http://localhost:8000/'
+
+
+# Service
+app = Flask(__name__)
+
+# /wallet route to verify our wallet
+@app.route('/wallet', methods=['GET'])
+def get_wallet():
+	return jsonify({'wallet': wallet.get_payout_address()})
+
+
+# /send-vote route to submit a vote
+@app.route('/send-vote', methods=['POST'])
+def sendvote():
+	if not request.json or not 'voters_id' in request.json:
+		abort(400)
+	voters_id = request.json['voters_id']
+	vote = {
+	  'id': voters_id,
+	  'submitted_to': play(voters_id),
+	  'done': True
+	}
+	return jsonify(vote)
+
+def play(voter_id_from_web):
+    ans = str(voter_id_from_web)
+    sel_url = server_url + 'write-vote?id={0}'
+    answer = requests.get(url=sel_url.format(ans))
+    return answer.text
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=8008)
---
+++ src/connector.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import logging
+
+from flask import Flask
+from flask import request
+
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+@app.route('/write-vote')
+@payment.required(1)
+def answer_question():
+	# extract answer from client request
+	answer = request.args.get('id')
+	voted_id = "Vote OK: " + answer + " to wallet: " + wallet.get_payout_address()
+	logging.info(voted_id)
+	print(voted_id)
+	return wallet.get_payout_address()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
